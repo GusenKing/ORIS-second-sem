e1cee9b4b5974be60137c64e4d9b1a70b1ee5df8
Emin <emin.sarux2004@yandex.ru>
2024-02-18 11:10:25 +0300

    sync

--- Pokemons/pokemons/src/App.js
+++ Pokemons/pokemons/src/App.js
@@ -3,43 +3,52 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+  const arr = [
+      {
+          name: "abc"
+      },
+      {
+          name: "abc2"
+      },
+      {
+          name: "abc3"
+      }
+  ]
 
-  useEffect(() =>
-  {
-      fetchKantoPokemon();
+  useEffect(() => {
+        setPokemons(fetchKantoPokemon());
       }, []);
-        return (
+  return (
       <>
+          <div>
+              Pokemons
+          </div>
+          <div>
+              {/*{arr.map((ob) => (<PokemonCard pokemon={ob}/>))}*/}
+              {pokemons.map((pokemon) => (<PokemonCard key={pokemon.id} pokemon={pokemon}/>))}
+          </div>
       </>
 );
 }
 
 function PokemonCard(props){
+    debugger;
     return(
         <>
-            <p>{}</p>
+            <p>{props.pokemon.name}</p>
         </>
     );
 }
 
-function fetchKantoPokemon()
-{
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+function fetchKantoPokemon(){
+    let data = [];
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
         .then(response => response.json())
-        .then(function(allpokemon){
-            allpokemon.results.forEach(function(pokemon){
-                fetchPokemonData(pokemon);
-            })
-        })
-}
-
-function fetchPokemonData(pokemon){
-    let url = pokemon.url;
-    fetch(url)
-        .then(response => response.json())
-        .then(function(pokemonData){
-            console.log(pokemonData)
-        })
+        .then(allpokemon => allpokemon.results.forEach(pokemon =>
+            fetch(pokemon.url)
+                .then(response => response.json())
+                .then(pokemonData => data.push(pokemonData))));
+    return data;
 }
 
 
